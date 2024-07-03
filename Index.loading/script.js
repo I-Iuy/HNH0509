5ddba56fb5925235e7737be7cae926d58fc08440
NguyenQuocHuy <quochuynguyen00397@gmail.com>
2024-07-04 01:09:01 +0700

    Initial commit

---
+++ Index.loading/script.js
@@ -0,0 +1,49 @@
+let isFirefox = typeof InstallTrigger !== 'undefined';
+const words = "Hoàng Ngọc Hà";
+
+let ANGLE = 360;
+const ANIMATION_DURATION = 4000;
+
+const animation = () => {
+  ANGLE -= 1; // Incremento do ângulo
+  document.querySelectorAll(".spiral *").forEach((el, i) => {
+    
+    const translateY = Math.sin(ANGLE * (Math.PI / 120)) * 100;
+    const scale = Math.cos(ANGLE * (Math.PI / 120)) * 0.5 + 0.5;
+    
+    const offset = parseInt(el.dataset.offset);
+    const delay = i * (ANIMATION_DURATION / 16) - offset;
+
+    setTimeout(() => {
+      el.style.transform = `translateY(${translateY}px) scale(${scale})`;
+    }, delay);
+  });
+
+  requestAnimationFrame(animation);
+};
+
+const characters = words.split("").forEach((char, i) => {
+  const createElement = (offset) => {
+    const div = document.createElement("div");
+    div.innerText = char;
+    div.classList.add("character");
+    div.setAttribute("data-offset", offset);
+    div.style.animationDelay = `-${i * (ANIMATION_DURATION / 16) - offset}ms`;
+    return div;
+  };
+
+  document.querySelector("#spiral").append(createElement(0));
+  document.querySelector("#spiral2").append(createElement((isFirefox ? 1 : -1) * (ANIMATION_DURATION / 2)));
+});
+
+// Chuyển trang set 5s
+setTimeout(() => {
+  const destination = localStorage.getItem('destination') || 'intro'; 
+  localStorage.removeItem('destination'); 
+
+  if (destination === 'intro') {
+    window.location.href = "../Intro.content/intro.html"; 
+  } else if (destination === 'main') {
+    window.location.href = "../Main.content/main.html";
+  }
+}, 8000);
---
+++ Intro.content/script.js
@@ -0,0 +1,49 @@
+var currentIndex = 0;
+var isFinished = false;
+window.onload = function () {
+  var lyrics = document.querySelector(".lyrics-large");
+  var popup = document.querySelector(".lyrics-box");
+
+  var fontSize = 30;
+  while (lyrics.scrollHeight > lyrics.offsetHeight) {
+    fontSize -= 1;
+    lyrics.style.fontSize = fontSize + "px";
+  }
+
+  var maxWidth = Math.max(lyrics.offsetWidth, lyrics.scrollWidth);
+  popup.style.width = maxWidth + "px";
+}
+
+function closeAlert() {
+  var lyrics = document.querySelector(".lyrics-large");
+  var songLines = [
+    "Bé lại chuẩn bị bước vào mùa thi áp lực rồi",
+    "Bo không biết làm sao để giúp bé bớt căng thẳng hơn </br></br><img src='https://i.pinimg.com/564x/6a/8e/38/6a8e382ac47e9a4abac382650b344427.jpg' alt='' width='200' height='200' style='border-radius: 20px;'/>",
+    "Hmm",
+    "Bo gửi tới em bé vài lời chúc động viên </br></br><img src='https://i.pinimg.com/736x/19/de/03/19de03fa835209e2ecd01e3c63859a5c.jpg' alt='' width='200' height='200' style='border-radius: 20px;'/> ",
+    "Click tiếp đi",
+    "Sắp thấy rùi click tiếp đi",
+    "Click thêm lần nữa chắc không làm em bé giận đâu",
+    "Click lần cuối"
+
+  ];
+
+  // Update lyrics content if currentIndex is within bounds
+  if (currentIndex < songLines.length) {
+    lyrics.innerHTML = songLines[currentIndex];
+    currentIndex++;
+  }
+
+  // Check if reached end of songLines
+  if (currentIndex >= songLines.length) {
+    if (!isFinished) {
+      isFinished = true; // Set flag to true after reading the last line
+    } else {
+      // Redirect user to index.html and set next destination to 'main'
+      localStorage.setItem('destination', 'main');
+      window.location.href = "../Index.loading/"; // Replace with your desired URL
+    }
+  }
+}
+
+
